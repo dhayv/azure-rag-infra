d86e98a5bf93e241cd798c5042579213f82a55f3
Hayvid <davidhyppolitedh@gmail.com>
2025-10-09 20:13:32 -0400

    Add terraform providers

---
+++ terraform/providers.tf
@@ -0,0 +1,25 @@
+# Configure the Azure provider
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0.2"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+data "azurerm_resource_group" "rg" {
+  name = var.rg_name
+}
+
+data "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = data.azurerm_resource_group.rg
+}
+
