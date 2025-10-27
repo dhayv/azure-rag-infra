653cd830f40977c682dba3aae3b1c1e9b4454b37
Hayvid <davidhyppolitedh@gmail.com>
2025-10-27 09:00:54 -0500

    fix(terraform): refactor GitOps bootstrap configuration
    - Add Kubernetes provider with dynamic AKS config
    - Fix Helm provider configuration (block syntax)
    - Remove hardcoded kubeconfig path
    - Remove hardcoded ACR credentials
    - Remove redundant ArgoCD application resource
    - Fix resource group data source reference

--- terraform/git-ops-bootstrap.tf
+++ terraform/git-ops-bootstrap.tf
@@ -1,24 +1,25 @@
 data "azurerm_kubernetes_cluster" "main" {
   name                = var.aks_name
-  resource_group_name = var.rg_name
+  resource_group_name = data.azurerm_resource_group.rg.name
 }
 
 
+
+provider "kubernetes" {
+  host                   = data.azurerm_kubernetes_cluster.main.kube_config.0.host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
+}
 
 provider "helm" {
-  kubernetes = {
-    config_path = "~/.kube/config"
+  kubernetes {
+    host                   = data.azurerm_kubernetes_cluster.main.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.main.kube_config.0.cluster_ca_certificate)
   }
-
-  registries = [
-    {
-      url      = var.acr_name
-      username = "username"
-      password = "password"
-    }
-  ]
 }
-
 
 resource "kubernetes_namespace" "argocd" {
   metadata {
@@ -31,93 +32,10 @@ resource "kubernetes_namespace" "argocd" {
 }
 
 resource "helm_release" "argocd" {
-  name        = "argocd"
-  namespace   = kubernetes_namespace.argocd.metadata[0].name
-  repository  = "https://argoproj.github.io/argo-helm"
-  version     = "1.2.3"
-  chart       = "argo-cd"
-  values     = [file("${path.module}/values/argocd-values.yaml")]
-}
-
-
-# Exposed ArgoCD API - authenticated using `username`/`password`
-provider "argocd" {
-  server_addr = "argocd.local:443"
-  username    = "foo"
-  password    = local.password
-}
-
-
-resource "argocd_application" "kustomize" {
-  metadata {
-    name      = "kustomize-app"
-    namespace = "argocd"
-    labels = {
-      test = "true"
-    }
-  }
-
-  spec {
-    project = "myproject"
-
-    source {
-      repo_url        = "https://github.com/dhayv/azure-rag-infra"
-      path            = "examples/helloWorld"
-      target_revision = "master"
-      kustomize {
-        name_prefix = "foo-"
-        name_suffix = "-bar"
-        images      = ["hashicorp/terraform:light"]
-        common_labels = {
-          "this.is.a.common" = "la-bel"
-          "another.io/one"   = "true" 
-        }
-      }
-    }
-
-    destination {
-      server    = "https://kubernetes.default.svc"
-      namespace = "foo"
-    }
-
-    sync_policy {
-      automated = {
-        prune       = true
-        self_heal   = true
-        allow_empty = true
-      }
-      # Only available from ArgoCD 1.5.0 onwards
-      sync_options = ["Validate=false"]
-      retry {
-        limit   = "5"
-        backoff = {
-          duration     = "30s"
-          max_duration = "2m"
-          factor       = "2"
-        }
-      }
-    }
-
-    ignore_difference {
-      group         = "apps"
-      kind          = "Deployment"
-      json_pointers = ["/spec/replicas"]
-    }
-
-    ignore_difference {
-      group         = "apps"
-      kind          = "StatefulSet"
-      name          = "someStatefulSet"
-      json_pointers = [
-        "/spec/replicas",
-        "/spec/template/spec/metadata/labels/bar",
-      ]
-      # Only available from ArgoCD 2.1.0 onwards
-      jq_path_expressions = [
-        ".spec.replicas",
-        ".spec.template.spec.metadata.labels.bar",
-      ]
-    }
-  }
+  name       = "argocd"
+  namespace  = kubernetes_namespace.argocd.metadata[0].name
+  repository = "https://argoproj.github.io/argo-helm"
+  version    = "1.2.3"
+  chart      = "argo-cd"
 }
 
